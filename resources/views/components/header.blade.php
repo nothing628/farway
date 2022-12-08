06835223f0d5818a3813e08a2ef3bc43d152ef71
nothing628 <yogyphang@gmail.com>
2022-12-08 12:42:14 +0700

    add header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,79 @@
+<div class="relative">
+    <div class="absolute bg-none p-4 z-20 top-0 left-0 right-0 box-border">
+        <div>
+            <ul class="block list-none py-0.5 px-0">
+                <li class="relative mr-4 float-left list-none w-auto">
+                    <a href="#" class="text-white">
+                        <i class="fa fa-bars fa-fw" aria-hidden="true"></i><span>Quick links</span>
+                    </a>
+                    <div class="hidden">
+                        <div class="pointer">
+                            <div class="pointer-inner"></div>
+                        </div>
+                        <ul class="dropdown-contents" role="menu">
+                            <li class="separator"></li>
+                            <li>
+                                <a href="./search.php?search_id=unanswered&amp;sid=00f3fb17f9b44bc12f621650899d62d4"
+                                    role="menuitem">
+                                    <i class="icon fa-file-o fa-fw" aria-hidden="true"></i><span>Unanswered
+                                        topics</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="./search.php?search_id=active_topics&amp;sid=00f3fb17f9b44bc12f621650899d62d4"
+                                    role="menuitem">
+                                    <i class="icon fa-file-o fa-fw" aria-hidden="true"></i><span>Active topics</span>
+                                </a>
+                            </li>
+                            <li class="separator"></li>
+                            <li>
+                                <a href="./search.php?sid=00f3fb17f9b44bc12f621650899d62d4" role="menuitem">
+                                    <i class="icon fa-search fa-fw" aria-hidden="true"></i><span>Search</span>
+                                </a>
+                            </li>
+                            <li class="separator"></li>
+                            <li>
+                                <a href="./memberlist.php?sid=00f3fb17f9b44bc12f621650899d62d4" role="menuitem">
+                                    <i class="icon fa-group fa-fw" aria-hidden="true"></i><span>Members</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="./memberlist.php?mode=team&amp;sid=00f3fb17f9b44bc12f621650899d62d4"
+                                    role="menuitem">
+                                    <i class="icon fa-shield fa-fw" aria-hidden="true"></i><span>The team</span>
+                                </a>
+                            </li>
+                            <li class="separator"></li>
+
+                        </ul>
+                    </div>
+                </li>
+                <li class="relative mr-4 float-left list-none w-auto">
+                    <a href="#" class="text-white">
+                        <i class="fa fa-question-circle fa-fw" aria-hidden="true"></i><span>FAQ</span>
+                    </a>
+                </li>
+                <li class="relative mr-4 float-right list-none w-auto">
+                    <a href="#" class="text-white">
+                        <i class="fa fa-power-off fa-fw" aria-hidden="true"></i><span>Login</span>
+                    </a>
+                </li>
+                <li class="relative mr-4 float-right list-none w-auto">
+                    <a href="#" class="text-white">
+                        <i class="fa fa-pencil-square-o fa-fw" aria-hidden="true"></i><span>Register</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="relative text-white bg-gradient-to-r from-[rgba(234,56,77,1)] to-[rgba(211,16,39,1)]">
+        <div class="block">
+            <div class="relative w-full h-[251px] text-center z-10 overflow-hidden">
+                <div class="w-[277px] my-[100px] mx-auto">
+                    <a><span class="text-5xl font-bold">FARWAY</span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-        <link rel="stylesheet" href="{{asset('css/fonts.css')}}" />
-        @vite('resources/css/app.css')
-    </head>
-    <body class="antialiased">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <link rel="stylesheet" href="{{ asset('css/fonts.css') }}" />
+    @vite('resources/css/app.css')
+</head>
+
+<body class="antialiased bg-[#ECF0F1]">
+    <div class="xl:w-[1200px] w-full mx-auto my-0 rounded-lg p-0">
+        @include('components.header')
+
         <h1 class="text-red-500">Nothing</h1>
-    </body>
+    </div>
+</body>
+
 </html>
